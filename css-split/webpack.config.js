50d34b5b2deeb478c0e9cecaef9a2434eb8fde12
yuanxujia <yuanxujia@meituan.com>
2017-10-11 20:18:09 +0800

    首页文件

---
+++ css-split/webpack.config.js
@@ -0,0 +1,15 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader']
+                })
+            }
+        ]
+    }
+};
